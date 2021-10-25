aba690a2c89b30bf2a70a19e80389069c09fd013
tanvi-15 <tanvideshpandedc@gmail.com>
2021-10-26 00:39:03 +0530

    About component added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import About from "../components/About/About";
 import HomePage from "../components/HomePage/HomePage";
 
 interface HomeProps {}
 
 const Home = ({}: HomeProps) => {
   return (
+    <div>
     <div className="d-flex vh-100 justify-content-center align-items-center">
-      <HomePage name="java not useful" />;
+      <HomePage name="java not useful" />
+      
+    </div>
+    <About/>
     </div>
   );
 };
