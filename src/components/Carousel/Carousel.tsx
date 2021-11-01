4d7a4a19a88839f5b2dd301b029a484ea2a255a5
jaymalave <jaym.github@gmail.com>
2021-11-02 00:06:16 +0530

    added coverflow

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,25 +1,36 @@
 import React from "react";
 import Slider from "react-touch-drag-slider";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation } from "swiper";
-import "swiper/swiper-bundle.min.css";
+import Coverflow from "react-coverflow";
+import { StyleRoot } from 'radium';
 
 import { images } from "../../data/carouselData";
 
 const Carousel = () => {
-  SwiperCore.use([Navigation]);
   return (
-    <div className="vh-100">
-      <Swiper navigation={true} className="mySwiper text-center">
-        {images.map(({ url, title }, index) => (
-          <SwiperSlide>
-            <div>
-              <img src={url} key={index} height="200"></img>
-              <h3>{title}</h3>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className="vh-100 text-center">
+     <StyleRoot>
+    <Coverflow
+      displayQuantityOfSide={2}
+      navigation
+      infiniteScroll
+      enableHeading
+      media={{
+        '@media (max-width: 768px)': {
+          width: '100vw',
+          height: '500px'
+        },
+        '@media (min-width: 768px)': {
+          width: '100vw',
+          height: '500px'     
+        }
+      }}
+    >
+      <img src='/assets/images/events/Events.png' data-action="https://facebook.github.io/react/" height="175"/>
+      <img src='/assets/images/events/Informals.png' data-action="http://passer.cc" height="175"/>
+      <img src='/assets/images/events/internship_expo.png' data-action="https://doce.cc/" height="175"/>
+      <img src='/assets/images/events/parvaah.png' data-action="http://tw.yahoo.com" height="175"/>
+    </Coverflow>
+  </StyleRoot>
     </div>
   );
 };
