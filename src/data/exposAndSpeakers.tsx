6a01c3298ecc355a90c396c2724af632167bc5c5
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-11-13 18:34:24 +0530

    parvaah ka page

--- src/data/exposAndSpeakers.tsx
+++ src/data/exposAndSpeakers.tsx
@@ -3,26 +3,19 @@ export const exposAndSpeakersDataJson = [
     linkHash: "tech",
     linkName: "Tech Expo",
     heading: "Tech Expo",
-    paraText: `A display of the most innovative technologies from around the
-              country, ranging from humanoid robots to high-speed drones, Tech
-              Expo is the most eye-catching event of Abhiyantriki.
-              Exhibits from some big names like DRDO, BARC, IMD, DAE and
-              many more at the tech expo are sure to ignite a fire for science and
-              technology and leave you inquisitive!`,
+    paraText: `Tech Expo is a unique exhibition and the most anticipated event at Abhiyantriki. With innovative technologies from all over the country, the Tech Expo contains exhibits from well-known organisations namely DRDO, ISRO, IMD, DAE and innovations ranging from speed-defying drones to humanoid robots with intellect on par with a human being. `,
     buttonHref: "#",
     buttonText: "Register",
     cards: [
       {
-        heading: "Mukesh Jain",
-        text: "CTIO and VP Insights and Data at Capgemini India, a global leader in consulting, digital transformation, technology and engineering services. A Techno-biz leader with over 25 years of experience in Digital Transformation, Big Data analytics, AI, IoT, Blockchains and so much more!",
-        image:
-          "https://kjsce-abhiyantriki.org/2020/img/Panel%20Session/mukesh%20jain.png",
+        heading: "Avishkar Hyperloop",
+        text: "The idea for this student project originated at IIT-Madras' Center of Innovation and has been conceptualised as India's first-ever Hyperloop endeavour. The Avishkar 'Garv,' built during the tumultuous times of the pandemic, is the pinnacle of current pod technology and the pride of India. In 2019, Team Avishkar's Hyperloop pod prototype finished in the top ten of the prestigious SpaceX Hyperloop Pod Competition, out of over 1600 teams worldwide. They were also the only Asian team at the competition finale, which took place at SpaceX's headquarters in California.",
+        image: "",
       },
       {
-        heading: "Tamaghna Basu",
-        text: "Co-founder and CTO at neoEYED, an AI monitoring 100+ behavioural factors to detect anomalies and prevent account takeover in real-time! A mentor for SANS and Stanford Cyber Security courses, he is also a Startup advisor!",
-        image:
-          "https://kjsce-abhiyantriki.org/2020/img/Panel%20Session/tamaghna%20basu.png",
+        heading: "RiverRecycle OY",
+        text: "RiverRecycle was founded in Finland halfway around the world to create a circular economy in every nation. RiverRecycle developed technology that effectively cleans rivers of plastic pollution. With over 500 installations planned in nine countries, they hope to prevent more than 60% of plastic river waste from entering our marine ecosystem. This will be a commercial endeavour, converting plastic waste into marketable products through collection and pyrolysis.",
+        image: "",
       },
     ],
   },
---
+++ src/pages/Parvaah.tsx
@@ -0,0 +1,119 @@
+import TextAndButton from "../components/TextAndButton/TextAndButton";
+import TwinkleTwinkle from "../components/TwinkleTwinkle/TwinkleTwinkle";
+
+interface ParvaahProps {}
+
+const Parvaah = ({}: ParvaahProps) => {
+  return (
+    <div>
+      <div className="container mb-5">
+        <div className="col">
+          {/* <TwinkleTwinkle title="Parvaah" /> */}
+          <img
+            src={process.env.PUBLIC_URL + "/assets/gifs/parvaah.gif"}
+            style={{ width: "100vw", margin: "0 auto" }}
+            alt=""
+          />
+
+          <div className="d-flex justify-content-around align-items-center text-center mb-5 parvaah">
+            <img
+              src="/assets/logos/Parvaah Logo.png"
+              alt=""
+              className="parvaah logo"
+            />
+            <div className="vl"></div>
+            <img
+              src="/assets/logos/Sustainable Reality logo.png"
+              alt=""
+              className="parvaah logo"
+            />
+          </div>
+
+          <TextAndButton
+            paraText="Parvaah, the social wing of K.J Somaiya College of Engineering works
+            towards the social causes of the society and improving the
+            conditions of the less privileged. Team Parvaah, through its various
+            initiatives, has been striving to spread maximum social awareness in
+            the community."
+            buttonText="Volunteer"
+            buttonHref="#"
+          />
+        </div>
+      </div>
+
+      <div>
+        {/* <!-- image left text right --> */}
+        <div className="container p-3 text-white">
+          <div className="row">
+            <div className="col-sm-6 col-md-6 flex-column d-flex justify-content-center">
+              <div className="align-items-center d-flex justify-content-center flex-column">
+                <div className="fs-1 fw-bold">Beach Clean-up (2017)</div>
+                <div
+                  className="align-items-center d-flex justify-content-center flex-column"
+                  style={{ fontSize: 20, fontFamily: "Roboto" }}
+                >
+                  Team Parvaah was a part of the Versova Beach clean up drive. A
+                  lot of student organisations across Mumbai joined hands to
+                  save the beach. Cleaning and hygiene issues have always been
+                  of utmost importance in our city.
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-6">
+              <div
+                style={{
+                  marginTop: "30",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <img
+                  style={{ width: "50%" }}
+                  src="/assets/images/events/Events.png"
+                  alt="Beach Clean-up"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <!-- image right text left --> */}
+        <div className="container p-3 text-white">
+          <div className="row">
+            <div className="col-sm-6 col-md-6 flex-column d-flex justify-content-center order-md-2">
+              <div className="align-items-center d-flex justify-content-center flex-column">
+                <div className="fs-1 fw-bold">Beach Clean-up (2017)</div>
+                <div
+                  className="align-items-center d-flex justify-content-center flex-column"
+                  style={{ fontSize: 20, fontFamily: "Roboto" }}
+                >
+                  Team Parvaah was a part of the Versova Beach clean up drive. A
+                  lot of student organisations across Mumbai joined hands to
+                  save the beach. Cleaning and hygiene issues have always been
+                  of utmost importance in our city.
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-6 order-md-1">
+              <div
+                style={{
+                  marginTop: 30,
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <img
+                  style={{ width: "50%" }}
+                  src="../assets/images/events/Events.png"
+                  alt="Beach Clean-up"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Parvaah;
